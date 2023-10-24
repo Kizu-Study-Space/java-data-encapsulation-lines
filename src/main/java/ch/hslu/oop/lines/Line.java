7d08629d009d7dad569c70a711848af6a5ab10e2
zorafuchs <13620981+zorafuchs@users.noreply.github.com>
2023-10-24 16:14:45 +0200

    Solve exercise d-e (not sure about f-h)

--- src/main/java/ch/hslu/oop/lines/Line.java
+++ src/main/java/ch/hslu/oop/lines/Line.java
@@ -1,8 +1,8 @@
 package ch.hslu.oop.lines;
 
 public class Line {
-    private final Point startPoint;
-    private final Point endPoint;
+    private Point startPoint;
+    private Point endPoint;
 
     public Line(final int x1, final int y1, final int x2, final int y2) {
         startPoint = new Point(x1, y1);
@@ -15,5 +15,21 @@ public class Line {
 
     public Point getEndPoint() {
         return endPoint;
+    }
+
+    public void setStartPoint(final Point point) {
+        this.startPoint = point;
+    }
+
+    public void setStartPoint(final int x, final int y) {
+        this.setStartPoint(new Point(x, y));
+    }
+
+    public void setEndPoint(final Point point) {
+        this.endPoint = point;
+    }
+
+    public void setEndPoint(final int x, final int y) {
+        this.setEndPoint(new Point(x, y));
     }
 }
--- src/test/java/ch/hslu/oop/lines/LineTest.java
+++ src/test/java/ch/hslu/oop/lines/LineTest.java
@@ -17,4 +17,28 @@ public class LineTest {
         assertEquals(3, line.getEndPoint().getX());
         assertEquals(4, line.getEndPoint().getY());
     }
+
+    @Test
+    public void setStartPoint() {
+        Line line = new Line(1, 2, 3, 4);
+        line.setStartPoint(new Point(5, 6));
+        assertEquals(5, line.getStartPoint().getX());
+        assertEquals(6, line.getStartPoint().getY());
+
+        line.setStartPoint(7, 8);
+        assertEquals(7, line.getStartPoint().getX());
+        assertEquals(8, line.getStartPoint().getY());
+    }
+
+    @Test
+    public void setEndPoint() {
+        Line line = new Line(1, 2, 3, 4);
+        line.setEndPoint(new Point(5, 6));
+        assertEquals(5, line.getEndPoint().getX());
+        assertEquals(6, line.getEndPoint().getY());
+
+        line.setEndPoint(7, 8);
+        assertEquals(7, line.getEndPoint().getX());
+        assertEquals(8, line.getEndPoint().getY());
+    }
 }
